7eaac27bba426c4a9f2b5e3ada15e711768a5c81
jesparzaronda <joseesparzaronda@gmail.com>
2019-01-13 11:08:36 +0100

    Add files via upload

---
+++ bbdd.py
@@ -0,0 +1,19 @@
+
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+
+
+import sys
+import os
+
+os.system("apt update")
+os.system("apt -y install mariadb-server")
+os.system("sed -i -e 's/bind-address.*/bind-address=0.0.0.0/' -e 's/utf8mb4/utf8/' /etc/mysql/mariadb.conf.d/50-server.cnf")
+os.system("systemctl restart mysql")
+os.system("mysqladmin -u root password xxxx")
+os.system("mysql -u root --password='xxxx' -e echo \"CREATE USER 'quiz' IDENTIFIED BY 'xxxx';\"")
+os.system("mysql -u root --password='xxxx' -e echo \"CREATE DATABASE quiz;\"")
+os.system("mysql -u root --password='xxxx' -e echo \"GRANT ALL PRIVILEGES ON quiz.* to 'quiz'@'localhost' IDENTIFIED by 'xxxx';\"")
+os.system("mysql -u root --password='xxxx' -e echo \"GRANT ALL PRIVILEGES ON quiz.* to 'quiz'@'%' IDENTIFIED by 'xxxx';\"")
+os.system("mysql -u root --password='xxxx' -e echo \"FLUSH PRIVILEGES;\"")
+
