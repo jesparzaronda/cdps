ee7b713426bbd2e37df8a299ced2eb9f17a430f6
jesparzaronda <joseesparzaronda@gmail.com>
2019-01-13 13:26:05 +0100

    Update bbdd.py

--- bbdd.py
+++ bbdd.py
@@ -2,17 +2,17 @@
 # -*- coding: latin-1 -*-
 
 import sys
-import subprocess
+import os
 
-subprocess.call("sudo apt-get update", shell=True)
-subprocess.call("sudo apt-get -y install mariadb-server", shell=True)
-subprocess.call("sudo sed -i -e 's/bind-address.*/bind-address=0.0.0.0/' -e 's/utf8mb4/utf8/' /etc/mysql/mariadb.conf.d/50-server.cnf", shell=True
+os.system("sudo apt-get update")
+os.system("sudo apt-get -y install mariadb-server")
+os.system("sudo sed -i -e 's/bind-address.*/bind-address=0.0.0.0/' -e 's/utf8mb4/utf8/' /etc/mysql/mariadb.conf.d/50-server.cnf")
 
-subprocess.call("sudo mysqladmin -u root password xxxx", shell=True)
-subprocess.call("sudo mysql -u root --password='xxxx' -e \"echo CREATE USER 'quiz' IDENTIFIED BY 'xxxx';\"", shell=True)
-subprocess.call("sudo mysql -u root --password='xxxx' -e \"echo CREATE DATABASE quiz;\"", shell=True)
-subprocess.call("sudo mysql -u root --password='xxxx' -e \"echo GRANT ALL PRIVILEGES ON quiz.* to 'quiz'@'localhost' IDENTIFIED by 'xxxx';\"", shell=True)
-subprocess.call("sudo mysql -u root --password='xxxx' -e \"echo GRANT ALL PRIVILEGES ON quiz.* to 'quiz'@'%' IDENTIFIED by 'xxxx';\"", shell=True)
-subprocess.call("sudo mysql -u root --password='xxxx' -e \"echo FLUSH PRIVILEGES;\"", shell=True)
+os.system("sudo mysqladmin -u root password xxxx")
+os.system("sudo mysql -u root --password='xxxx' -e \"CREATE USER 'quiz' IDENTIFIED BY 'xxxx';\"")
+os.system("sudo mysql -u root --password='xxxx' -e \"CREATE DATABASE quiz;\"")
+os.system("sudo mysql -u root --password='xxxx' -e \"GRANT ALL PRIVILEGES ON quiz.* to 'quiz'@'localhost' IDENTIFIED by 'xxxx';\"")
+os.system("sudo mysql -u root --password='xxxx' -e \"GRANT ALL PRIVILEGES ON quiz.* to 'quiz'@'%' IDENTIFIED by 'xxxx';\"")
+os.system("sudo mysql -u root --password='xxxx' -e \"FLUSH PRIVILEGES;\"")
 
-subprocess.call("sudo systemctl restart mysql", shell=True)
+os.system("sudo systemctl restart mysql")
