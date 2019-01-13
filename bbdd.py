348d467f7b6624f73d0f3349ffa8286da8467507
jesparzaronda <joseesparzaronda@gmail.com>
2019-01-13 12:30:06 +0100

    Update bbdd.py

--- bbdd.py
+++ bbdd.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python
 # -*- coding: latin-1 -*-
 
-import sys
-import os
-os.system("sudo apt update")
-os.system("sudo apt -y install mariadb-server")
+os.system("sudo apt-get update")
+os.system("sudo apt-get upgrade")
+os.system("sudo apt-get install software-properties-common")
+os.system("ssudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8")
+os.system("sudo add-apt-repository 'deb [arch=amd64,arm64,ppc64el] http://tedeco.fi.upm.es/mirror/mariadb/repo/10.1/ubuntu bionic main'")
+
+os.system("sudo apt-get update")
+os.system("sudo apt-get -y install mariadb-server")
 os.system("sudo sed -i -e 's/bind-address.*/bind-address=0.0.0.0/' -e 's/utf8mb4/utf8/' /etc/mysql/mariadb.conf.d/50-server.cnf")
 
 os.system("sudo mysqladmin -u root password xxxx")
@@ -13,4 +17,5 @@ os.system("sudo mysql -u root --password='xxxx' -e echo \"CREATE DATABASE quiz;\
 os.system("sudo mysql -u root --password='xxxx' -e echo \"GRANT ALL PRIVILEGES ON quiz.* to 'quiz'@'localhost' IDENTIFIED by 'xxxx';\"")
 os.system("sudo mysql -u root --password='xxxx' -e echo \"GRANT ALL PRIVILEGES ON quiz.* to 'quiz'@'%' IDENTIFIED by 'xxxx';\"")
 os.system("sudo mysql -u root --password='xxxx' -e echo \"FLUSH PRIVILEGES;\"")
+
 os.system("sudo systemctl restart mysql")
