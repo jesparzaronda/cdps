0eeae06f7edc211c1b8540db1104f6443a83c776
jesparzaronda <joseesparzaronda@gmail.com>
2019-01-13 13:44:06 +0100

    Add files via upload

---
+++ cluster.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import time
+import sys
+import os
+import socket
+
+os.system(" sudo echo \"20.2.4.21 nas1\" >> /etc/hosts ")
+os.system(" sudo echo \"20.2.4.22 nas2\" >> /etc/hosts ")
+os.system(" sudo echo \"20.2.4.23 nas3\" >> /etc/hosts ")
