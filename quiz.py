f0b11944cd02db50e8349bbafa9d1d610c41cdca
jesparzaronda <joseesparzaronda@gmail.com>
2019-01-20 00:13:01 +0100

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import time
+import sys
+import os
+import socket
+
+
+print(" ---- CLONACION DEL QUIZ ----")
+os.system("git clone https://github.com/CORE-UPM/quiz_2019.git")
+
+print(" ---- INICIO INSTALACION NPM ----")
+os.system("cd quiz_2019")
+os.system("mkdir public/uploads")
+os.system("npm install")
+os.system("npm install forever")
+os.system("npm install mysql2")
