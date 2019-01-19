a5f4c592f7d8dd3b3308cdf3bced73daea0bc0f7
jesparzaronda <joseesparzaronda@gmail.com>
2019-01-19 19:38:50 +0100

    Update bbdd.py

--- bbdd.py
+++ bbdd.py
@@ -9,6 +9,7 @@ print(" ---- INSTALANDO MARIADB-SERVER ----")
 os.system("sudo apt-get update")
 os.system("sudo apt-get -y install mariadb-server")
 os.system("sudo sed -i -e 's/bind-address.*/bind-address=0.0.0.0/' -e 's/utf8mb4/utf8/' /etc/mysql/mariadb.conf.d/50-server.cnf")
+os.system("sudo systemctl restart mysql")
 
 os.system("sudo mysqladmin -u root password xxxx")
 os.system("sudo mysql -u root --password='xxxx' -e \"CREATE USER 'quiz' IDENTIFIED BY 'xxxx';\"")
@@ -16,7 +17,5 @@ os.system("sudo mysql -u root --password='xxxx' -e \"CREATE DATABASE quiz;\"")
 os.system("sudo mysql -u root --password='xxxx' -e \"GRANT ALL PRIVILEGES ON quiz.* to 'quiz'@'localhost' IDENTIFIED by 'xxxx';\"")
 os.system("sudo mysql -u root --password='xxxx' -e \"GRANT ALL PRIVILEGES ON quiz.* to 'quiz'@'%' IDENTIFIED by 'xxxx';\"")
 os.system("sudo mysql -u root --password='xxxx' -e \"FLUSH PRIVILEGES;\"")
-
-os.system("sudo systemctl restart mysql")
 
 print(" ---- FIN INSTALACION MARIADB-SERVER ----")
