7d23aaad3d0fddd9fdc51901ba58ef0f1d44582f
jesparzaronda <joseesparzaronda@gmail.com>
2019-01-20 11:07:31 +0100

    Update lb.py

--- lb.py
+++ lb.py
@@ -11,9 +11,8 @@ os.system("sudo apt-get install haproxy")
 
 
 fichero = open("/etc/haproxy/haproxy.cfg", "a")
-fichero.write("frontend haproxynode \n bind *:80 \n mode http \n default_backend backendnodes \n")
-fichero.write("backend backendnodes \n balance roundrobin \n option forwardfor \n http-request set-header X-Forwarded-Port %[dst_port] \n http-request add-header X-Forwarded-Proto https if { ssl_fc } \n server s1 20.2.3.11:8080 check \n server s2 20.2.3.12:8080 check \n server s3 20.2.3.13:8080 check\n")
-fichero.write("listen stats \n bind :32700 \n stats enable \n stats uri / \n stats hide-version \n stats auth root:xxxx \n")
+fichero.write("frontend lb \n bind *:80 \n mode http \n default_backend webservers \n")
+fichero.write("backend webservers\n mode http \n balance roundrobin \n server s1 20.2.3.11:80 check \n server s2 20.2.3.12:80 check \n server s3 20.2.3.13:80 check \n")
 fichero.close()
 
 os.system("sudo service haproxy restart")
