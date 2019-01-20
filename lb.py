e8afae102f778963a3b8ed80dc76c5791ce77d20
jesparzaronda <joseesparzaronda@gmail.com>
2019-01-20 11:11:17 +0100

    Update lb.py

--- lb.py
+++ lb.py
@@ -12,7 +12,7 @@ os.system("sudo apt-get install haproxy")
 
 fichero = open("/etc/haproxy/haproxy.cfg", "a")
 fichero.write("frontend lb \n bind *:80 \n mode http \n default_backend webservers \n")
-fichero.write("backend webservers\n mode http \n balance roundrobin \n server s1 20.2.3.11:80 check \n server s2 20.2.3.12:80 check \n server s3 20.2.3.13:80 check \n")
+fichero.write("backend webservers\n mode http \n balance roundrobin \n server s1 20.2.3.11:3000 check \n server s2 20.2.3.12:3000 check \n server s3 20.2.3.13:3000 check \n")
 fichero.close()
 
 os.system("sudo service haproxy restart")
